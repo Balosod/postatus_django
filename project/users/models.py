ec74665683919b375ab9818831aecce1c4eb2448
dewll <sodiqb86@gmail.com>
2022-10-02 21:37:24 +0100

    initial commit

--- project/users/models.py
+++ project/users/models.py
@@ -26,3 +26,62 @@ class User(AbstractUser):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+class BaseModel(models.Model):
+    category = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    price = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    tags = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='upload_image')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+class Product(BaseModel):
+    what_to_sell = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "Product"
+
+    def _str_(self):
+        return self.what_to_sell
+
+class Service(BaseModel):
+    what_to_do = models.CharField(max_length=300)
+    delivery_type = models.CharField(max_length=300)
+    duration = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "Service"
+
+    def _str_(self):
+        return self.what_to_do
+    
+class Event(BaseModel):
+    what_is_it_about = models.CharField(max_length=300)
+    medium = models.CharField(max_length=300)
+    date_and_time = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "Event"
+
+    def _str_(self):
+        return self.what_is_it_about
+
+class Delivery(models.Model):
+    pick_up_location = models.CharField(max_length=300)
+    delivery_location = models.CharField(max_length=300)
+    category = models.CharField(max_length=100)
+    delivery_type = models.CharField(max_length=100)
+    set_price = models.CharField(max_length=30)
+    size = models.CharField(max_length=30)
+    select_category = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    tags = models.CharField(max_length=300)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.pick_up_location
