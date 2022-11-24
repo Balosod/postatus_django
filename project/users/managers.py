91dc7ea8e1935d8ef579951682cda3ddbef2913c
dewll <sodiqb86@gmail.com>
2022-11-24 22:57:41 +0100

    fix a bug

--- project/users/managers.py
+++ project/users/managers.py
@@ -26,6 +26,19 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
 
         return self._create_user(email, password, **extra_fields)
+    
+    
+    
+    def create_user_super(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        #user.username = email
+        user.set_password(password)
+        user.save()
+        return user
+
 
     def create_superuser(self, email,password = None, **extra_fields):
         """
@@ -39,4 +52,4 @@ class CustomUserManager(BaseUserManager):
             raise ValueError("Superuser must have is_staff=True")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
-        return self.create_user(email,password, **extra_fields)
+        return self.create_user_super(email,password, **extra_fields)
